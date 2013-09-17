c398e7cd8307fe66ce4b75f798cccd1e0e99a66d
alexander polzin <alexander@girls.io>
2013-09-17 11:00:14 +0200

    admin create

--- app/models/instagram_entry.rb
+++ app/models/instagram_entry.rb
@@ -1,2 +1,3 @@
 class InstagramEntry < ActiveRecord::Base
+  attr_accessible :latlng, :low_resolution_url, :low_resolution_width, :low_resolution_height, :standard_resolution_url, :standard_resolution_width, :standard_resolution_height, :user_full_name, :user_profile_picture, :user_username, :user_website
 end
